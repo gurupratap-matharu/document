416ed6f99c84a1346b652321bdb1c0bd36671652
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-12-30 14:20:49 -0300

    added filename propery

--- document.py
+++ document.py
@@ -1,3 +1,6 @@
+from cursor import Cursor
+
+
 class Document:
     """
     Represents a generic document that can be parsed by text editors or document
@@ -6,29 +9,38 @@ class Document:
 
     def __init__(self):
         self.characters = []
-        self.cursor = 0
-        self.filename = ''
+        self.cursor = Cursor(self)
+        self._filename = ''
 
     def insert(self, character):
         """
         Insert a new character in the document at the current cursor position
         """
-        self.characters.insert(self.cursor, character)
-        self.cursor += 1
+        self.characters.insert(self.cursor.position, character)
+        self.cursor.forward()
 
     def delete(self):
         """Deletes the character at current cursor position"""
-        del self.characters[self.cursor]
+        del self.characters[self.cursor.position]
 
     def save(self):
         """Saves the document to persistent memory storage on the computer"""
-        with open(self.filename, 'w') as f:
+        with open(self._filename, 'w') as f:
             f.write(''.join(self.characters))
 
-    def forward(self):
-        """Moves the cursor ahead by one unit"""
-        self.cursor += 1
+    @property
+    def string(self):
+        return ''.join(self.characters)
 
-    def back(self):
-        """Moves the cursor back by one unit"""
-        self.cursor -= 1
+    @property
+    def filename(self):
+        print('getting filename...')
+        return self._filename
+
+    @filename.setter
+    def filename(self, name):
+        print('setting filename...')
+        self._filename = name
+
+    def __repr__(self):
+        return self._filename
